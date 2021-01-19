7e7f476cfb978485d8e16eaf49ebfe984af7506f
tatycat <williams.tatyanna@gmail.com>
2021-01-19 06:13:26 -0500

    Added class method decorator to set up/down

--- HomePg_ToDo_TestSuite.py
+++ HomePg_ToDo_TestSuite.py
@@ -4,9 +4,12 @@ from time import sleep
 import unittest
 
 class ToDoMVCTest(unittest.TestCase):
+    @classmethod
     def setUP(self):    
-        browser = webdriver.Firefox()
-        browser.get('https://todomvc.com/examples/angular2/')
+        self.driver = webdriver.Firefox()
+        self.driver.implicitly_wait(30)
+        self.driver.maximize_window
+        self.driver.get('https://todomvc.com/examples/angular2/')
         
     #Add/type text in input box (not disabled)
     def test_text_in_input_box(self):
@@ -22,9 +25,10 @@ class ToDoMVCTest(unittest.TestCase):
 
     #refreshng the page should still leave the same incomplete items displayng prior to refresh
     def test_save_state_on_refresh():
-
-    def tEARDOWN(self):
-        self.driver.quit()
+    
+    @classmethod
+    def tEARDOWN(cls):
+        # self.driver.quit()
 
 if __name__ == '__main__':
     unittest.main()
