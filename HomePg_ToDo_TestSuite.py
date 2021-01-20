57adacee44f30abe80d4402ad8ff8cf97d5b1d1d
tatycat <williams.tatyanna@gmail.com>
2021-01-19 21:50:53 -0500

    wip - text in input box method

--- HomePg_ToDo_TestSuite.py
+++ HomePg_ToDo_TestSuite.py
@@ -7,31 +7,33 @@ class ToDoMVCTest(unittest.TestCase):
     @classmethod
     def setUP(self):    
         self.driver = webdriver.Firefox()
-        self.driver.implicitly_wait(30)
+        self.driver.implicitly_wait(15)
         self.driver.maximize_window
         self.driver.get('https://todomvc.com/examples/angular2/')
         
     #Add/type text in input box (not disabled)
     def test_text_in_input_box(self):
-        # locate input box
-        # add characters to input box 
-        # 
+        # value
+        self.findElement(By.xpath("/html/body/todo-app/section/header/input")).sendKeys('test text')
+
 
     #can submit added text to create a checklist item
-    def test_submit_text():
+    # def test_submit_text():
 
     #checklist item can be marked complete only by toggle: body > todo-app > section > section > ul > li > div > input
-    def test_mark_complete():
+    # def test_mark_complete():
 
     #item marked complete has strikethrough on text && is greyed out
-    def test_item_shows_complete():
+    # def test_item_shows_complete():
 
     #refreshng the page should still leave the same incomplete items displayng prior to refresh
-    def test_save_state_on_refresh():
+    # def test_save_state_on_refresh():
+        # driver.navigate().refresh()
+
     
-    @classmethod
-    def tEARDOWN(cls):
-        cls.driver.quit()
+    # @classmethod
+    # def tEARDOWN(cls):
+        # cls.driver.quit()
 
 if __name__ == '__main__':
     unittest.main()
