04ebfbd3936c11d47fc222ed9eb03b58aec76dc8
tatycat <williams.tatyanna@gmail.com>
2021-02-16 23:57:10 -0500

    DriverWait Wait until input is visible in setUp

--- HomePg_ToDo_TestSuite.py
+++ HomePg_ToDo_TestSuite.py
@@ -1,6 +1,8 @@
 import unittest
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 from time import sleep
 
 class ToDoMVCTest(unittest.TestCase):
@@ -9,7 +11,13 @@ class ToDoMVCTest(unittest.TestCase):
     def setUpClass(self):    
         self.driver = webdriver.Chrome()
         self.driver.get('https://todomvc.com/examples/angular2/')
-        self.driver.implicitly_wait(15)
+        #Wait until input box is available in DOM tree. This was a previous Error/Break
+        # https://selenium-python.readthedocs.io/waits.html#explicit-waits
+        try:
+            element = WebDriverWait(self.driver, 10).until(EC.presence_of_element_located((By.XPATH, "/html/body/todo-app/section/header/input")))
+        except:
+            self.driver.quit()
+        # self.driver.implicitly_wait(15)
 
         print('----setupclass done!')
         
@@ -40,10 +48,10 @@ class ToDoMVCTest(unittest.TestCase):
         # driver.navigate().refresh()
 
     
-    # @classmethod
-    # def tearDownClass(cls):
-    #     # cls.driver.quit()
-    #     print ("yea")
+    @classmethod
+    def tearDownClass(self):
+        self.driver.quit()
+        print ("yea")
 
 if __name__ == '__main__':
     unittest.main()
