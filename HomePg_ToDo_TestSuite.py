8b3b7d062f8793ebd1b71b39e31afcf1725cc5ba
tatycat <williams.tatyanna@gmail.com>
2021-03-09 00:58:39 -0500

    @Ben, def test_mark_complete OG Error

--- HomePg_ToDo_TestSuite.py
+++ HomePg_ToDo_TestSuite.py
@@ -11,10 +11,11 @@ from time import sleep
 
 class ToDoMVCTest(unittest.TestCase):
 
-    @classmethod
-    def setUpClass(self):    
+   
+    def setUp(self):    
         self.driver = webdriver.Chrome()
         self.driver.get('https://todomvc.com/examples/angular2/')
+        
         #Wait until input box is available in DOM tree. This was a previous Error/Break
         # https://selenium-python.readthedocs.io/waits.html#explicit-waits
         try:
@@ -31,76 +32,45 @@ class ToDoMVCTest(unittest.TestCase):
         input_todo.send_keys('test text 2')
         input_todo.send_keys(Keys.ENTER)
 
-        # if test test is present: print success! 
-        # @BEN Should I print out status of each method/test? && having an error
-
-        # if self.driver.find_element_by_link_text("test text"):
-        #     print ("Condition SUCCESS: Add/type text in input box (not disabled)")
-        # else:
-        #     print("FAILURE: test_text_in_input_box")
-            # '''
-            # Traceback (most recent call last):
-            #     File "/Users/luckytaty/Documents/Code.Eat.Sleep/QA_Scripts/todoApp_Test/HomePg_ToDo_TestSuite.py", line 37, in test_text_in_input_box
-            #         if self.driver.find_element_by_link_text("test text"):
-            #     File "/usr/local/lib/python3.9/site-packages/selenium/webdriver/remote/webdriver.py", line 428, in find_element_by_link_text
-            #         return self.find_element(by=By.LINK_TEXT, value=link_text)
-            #     File "/usr/local/lib/python3.9/site-packages/selenium/webdriver/remote/webdriver.py", line 976, in find_element
-            #         return self.execute(Command.FIND_ELEMENT, {
-            #     File "/usr/local/lib/python3.9/site-packages/selenium/webdriver/remote/webdriver.py", line 321, in execute
-            #         self.error_handler.check_response(response)
-            #     File "/usr/local/lib/python3.9/site-packages/selenium/webdriver/remote/errorhandler.py", line 242, in check_response
-            #         raise exception_class(message, screen, stacktrace)
-            #     selenium.common.exceptions.NoSuchElementException: Message: no such element: Unable to locate element: {"method":"link text","selector":"test text"}
-            #     (Session info: chrome=88.0.4324.150)
-            # '''
-
-
 
     #checklist item can be marked complete only by toggle:
     def test_mark_complete(self):
             # find toggle, mark toggle, check that it shows as complete 
-        timeout = 15
 
-        try:
-            # attemtps to locate:
-            # toggle_check = WebDriverWait(self.driver,timeout).until(EC.presence_of_element_located((By.CSS_SELECTOR, "li#toggle" )))
-            # toggle_check = WebDriverWait(self.driver,timeout).until(EC.presence_of_element_located((By.CSS_SELECTOR, "toggle" )))
+        toggle_check = self.driver.find_element_by_xpath("/html/body/todo-app/section/section/ul/li/div/input")
+        toggle_check.click()
             # toggle_check = WebDriverWait(self.driver,timeout).until(EC.presence_of_element_located((By.CLASS_NAME, "toggle" )))            
             # toggle_check = WebDriverWait(self.driver,timeout).until(EC.presence_of_element_located((By.CSS_SELECTOR, "input#toggle" )))
-            # toggle_check = WebDriverWait(self.driver,timeout).until(EC.presence_of_element_located((By.CSS_SELECTOR, "input#toggle" )))
-            # toggle_check = WebDriverWait(self.driver, 20).until(EC.presence_of_element_located((By.CSS_SELECTOR, "input.toggle")))
-            # toggle_check = WebDriverWait(self.driver, timeout).until(EC.presence_of_element_located((By.CSS_SELECTOR, "input:contains('test text')")))
-            toggle_check = WebDriverWait(self.driver, timeout).until(EC.presence_of_element_located((By.XPATH, "/html/body/todo-app/section/section/ul/li/div/input")))
-
-
-            # todo-app > section > section > ul > li > div > input / css=tag#id
-            # css=tag:contains("inner text")
-
-
-            # toggle_check.click()
-        except TimeoutException:
-            print("Failed to locate toggle checkbox")
-
-        # toggle_check.click()
-        # if toggle_check.is_selected():
-            # print('test_mark_complete SELECTED')
-                # driver.find_element_by_id("firefox").get_attribute("checked")
-
-
-
-
-    #item marked complete has strikethrough on text && is greyed out
-    # def test_item_shows_complete():
-
-    #refreshng the page should still leave the same incomplete items displayng prior to refresh
-    # def test_save_state_on_refresh():
-        # driver.navigate().refresh()
+           
+        
+        if toggle_check.is_selected():
+            print('test_mark_complete SELECTED')
 
     
-    @classmethod
-    def tearDownClass(self):
-        self.driver.implicitly_wait(15)	
-        # print (" We did it Joe...")
+    # ERROR MESSAGE: 
+    #     Traceback (most recent call last):
+    #     line 40, in test_mark_complete
+    #        toggle_check = self.driver.find_element_by_xpath("/html/body/todo-app/section/section/ul/li/div/input")
+    #     File "/usr/local/lib/python3.9/site-packages/selenium/webdriver/remote/webdriver.py", line 394, in find_element_by_xpath
+    #         return self.find_element(by=By.XPATH, value=xpath)
+    #     File "/usr/local/lib/python3.9/site-packages/selenium/webdriver/remote/webdriver.py", line 976, in find_element
+    #         return self.execute(Command.FIND_ELEMENT, {
+    #     File "/usr/local/lib/python3.9/site-packages/selenium/webdriver/remote/webdriver.py", line 321, in execute
+    #         self.error_handler.check_response(response)
+    #     File "/usr/local/lib/python3.9/site-packages/selenium/webdriver/remote/errorhandler.py", line 242, in check_response
+    #         raise exception_class(message, screen, stacktrace)
+
+    #     selenium.common.exceptions.NoSuchElementException: Message: no such element: Unable to locate element: {"method":"xpath","selector":"/html/body/todo-app/section/section/ul/li/div/input"}
+    #     (Session info: chrome=89.0.4389.82)
+
+
+
+
+
+    
+    # @classmethod
+    def tearDown(self):
+        # self.driver.implicitly_wait(15)	
         self.driver.quit()
 
 if __name__ == '__main__':
@@ -113,6 +83,6 @@ if __name__ == '__main__':
 
 #REMINDER
 # after one (1) hour of troubleshooting, branch, commit & @ Ben 
-# """@BEN, I keep receiving this error: 
+# """&& @ BEN, I keep receiving this error: 
 
 #   """
--- README.md
+++ README.md
@@ -1,7 +1,8 @@
 ACTIVATE PYTHON ENV
 source todo-env/bin/activate  
 
-Update/Install Selenium - Webdriver
+Error: "This version of ChromeDriver only supports Chrome version 87"
+Solution: Update/Install Selenium - Webdriver
     brew upgrade chromedriver
     python -m pip install pip
 <!-- *history* shows last used commands-->
