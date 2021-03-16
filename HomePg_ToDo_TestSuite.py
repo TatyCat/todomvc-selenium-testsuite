6b6230fd242712dc116321783dd67c02498aaf17
tatycat <williams.tatyanna@gmail.com>
2021-03-15 23:24:41 -0400

    silo methods/cleanup

--- HomePg_ToDo_TestSuite.py
+++ HomePg_ToDo_TestSuite.py
@@ -33,44 +33,24 @@ class ToDoMVCTest(unittest.TestCase):
         input_todo.send_keys(Keys.ENTER)
 
 
-    #checklist item can be marked complete only by toggle:
+        #checklist item can be marked complete only by toggle:
     def test_mark_complete(self):
             # find toggle, mark toggle, check that it shows as complete 
+        input_todo = self.driver.find_element_by_xpath("/html/body/todo-app/section/header/input")
+        input_todo.send_keys('test text')
+        input_todo.send_keys(Keys.ENTER)
 
         toggle_check = self.driver.find_element_by_xpath("/html/body/todo-app/section/section/ul/li/div/input")
         toggle_check.click()
-            # toggle_check = WebDriverWait(self.driver,timeout).until(EC.presence_of_element_located((By.CLASS_NAME, "toggle" )))            
-            # toggle_check = WebDriverWait(self.driver,timeout).until(EC.presence_of_element_located((By.CSS_SELECTOR, "input#toggle" )))
-           
         
         if toggle_check.is_selected():
             print('test_mark_complete SELECTED')
-
-    
-    # ERROR MESSAGE: 
-    #     Traceback (most recent call last):
-    #     line 40, in test_mark_complete
-    #        toggle_check = self.driver.find_element_by_xpath("/html/body/todo-app/section/section/ul/li/div/input")
-    #     File "/usr/local/lib/python3.9/site-packages/selenium/webdriver/remote/webdriver.py", line 394, in find_element_by_xpath
-    #         return self.find_element(by=By.XPATH, value=xpath)
-    #     File "/usr/local/lib/python3.9/site-packages/selenium/webdriver/remote/webdriver.py", line 976, in find_element
-    #         return self.execute(Command.FIND_ELEMENT, {
-    #     File "/usr/local/lib/python3.9/site-packages/selenium/webdriver/remote/webdriver.py", line 321, in execute
-    #         self.error_handler.check_response(response)
-    #     File "/usr/local/lib/python3.9/site-packages/selenium/webdriver/remote/errorhandler.py", line 242, in check_response
-    #         raise exception_class(message, screen, stacktrace)
-
-    #     selenium.common.exceptions.NoSuchElementException: Message: no such element: Unable to locate element: {"method":"xpath","selector":"/html/body/todo-app/section/section/ul/li/div/input"}
-    #     (Session info: chrome=89.0.4389.82)
-
-
-
 
 
     
     # @classmethod
     def tearDown(self):
-        # self.driver.implicitly_wait(15)	
+        self.driver.implicitly_wait(5)	
         self.driver.quit()
 
 if __name__ == '__main__':
--- README.md
+++ README.md
@@ -6,3 +6,7 @@ Solution: Update/Install Selenium - Webdriver
     brew upgrade chromedriver
     python -m pip install pip
 <!-- *history* shows last used commands-->
+
+run independent methods/test
+ python HomePg_ToDo_TestSuite.py ToDoMVCTest.test_mark_complete 
+
